90bf31bfc683161d09c22d8d6eb9285b48414175
Alessio Tabolacci <alessio.tabolacci@hotmail.com>
2022-02-04 23:48:58 +0100

    Sistemazione codice

--- src/main/java/it/contocorrente/entity/Movimento.java
+++ src/main/java/it/contocorrente/entity/Movimento.java
@@ -5,7 +5,6 @@ public class Movimento {
 
 	private String dataMovimento;
 	private double importo;
-	//private ContoCorrente conto;
 	private String iban;
 	private TipoOperazione tipo;
 
@@ -13,16 +12,15 @@ public class Movimento {
 	//Getters
 	public String getDataMovimento() { return dataMovimento; }
 	public double getImporto() { return importo; }
-	//public ContoCorrente getConto() { return conto; }
 	public String getIban() { return iban; }
 	public TipoOperazione getTipo() { return tipo; }
 
 	//Setters
 	public void setDataMovimento(String dataMovimento) { this.dataMovimento = dataMovimento; }
 	public void setImporto(double importo) { this.importo = importo; }
-	//public void setConto(ContoCorrente conto) { this.conto = conto; }
 	public void setIban(String iban) { this.iban = iban; }
 	public void setTipo(TipoOperazione tipo) { this.tipo = tipo; }
+	
 
 
 
